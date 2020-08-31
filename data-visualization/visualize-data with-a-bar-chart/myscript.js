853b7fe32739187f33d84ee13db006bde49dcced
mhatiboglu <mh1741mh@gmail.com>
2020-08-31 13:14:14 +0100

    create svg chart

--- data-visualization/visualize-data with-a-bar-chart/myscript.js
+++ data-visualization/visualize-data with-a-bar-chart/myscript.js
@@ -1,1 +1,12 @@
-document.getElementById('test').innerHTML= 'test22222'
+let yMargin = 50,
+  width = 1000,
+  height = 500,
+  barWidth = width / 275;
+
+//Crete svg chart
+
+let svgChart = d3.select(".chartHolder")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .style("background", "gray");
