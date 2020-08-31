8e9510882dccfb42308a136ac6b5826c27051f83
mhatiboglu <mh1741mh@gmail.com>
2020-08-31 13:19:56 +0100

    fetch raw data from url

--- data-visualization/visualize-data with-a-bar-chart/myscript.js
+++ data-visualization/visualize-data with-a-bar-chart/myscript.js
@@ -5,8 +5,20 @@ let yMargin = 50,
 
 //Crete svg chart
 
-let svgChart = d3.select(".chartHolder")
+let svgChart = d3
+  .select(".chartHolder")
   .append("svg")
   .attr("width", width)
   .attr("height", height)
   .style("background", "gray");
+
+// fetch raw data from url 
+let rawData;
+fetch(
+  "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+).then((response)=> {
+    response.text().then((text)=>{
+        rawData=text;
+        console.log(rawData);
+    })
+})
