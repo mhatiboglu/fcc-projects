8e9965defae20056e9067049c38c971eb1e53819
mhatiboglu <mh1741mh@gmail.com>
2020-09-21 21:36:09 +0100

    Create tooltip when mouse over and delete when out

--- data-visualization/visualize-data with-a-bar-chart/myscript.js
+++ data-visualization/visualize-data with-a-bar-chart/myscript.js
@@ -11,6 +11,12 @@ let svgChart = d3
   .attr("width", width + 100)
   .attr("height", height + 60)
   .style("background", "gray");
+// Create tooltip
+let tooltip = d3
+  .select(".chartHolder")
+  .append("div")
+  .attr("id", "tooltip")
+  .style("opacity", 0);
 
 let minDate, maxDate;
 // Get raw data from url
@@ -43,7 +49,8 @@ d3.json(
     let xAxisGroup = svgChart
       .append("g")
       .call(xAxis)
-      .attr("transform", "translate(50, 510)");
+      .attr("transform", "translate(50, 510)")
+      .attr("id", "x-axis");
 
     //////////////////////////////////
     ////////////  YSCALES  //////////
@@ -74,16 +81,35 @@ d3.json(
     let yAxisGroup = svgChart
       .append("g")
       .call(yAxis)
-      .attr("transform", "translate(50, 10)");
-     //////////////////////////////////
+      .attr("transform", "translate(50, 10)")
+      .attr("id", "y-axis");
+
+    //////////////////////////////////
     ////// CREATE BARS FOR GDP  //////
     /////////////////////////////////
+    let years = rawData.data.map((item) => {
+      let quarter;
+      let temp = item[0].substring(5, 7);
+
+      if (temp === "01") {
+        quarter = "Q1";
+      } else if (temp === "04") {
+        quarter = "Q2";
+      } else if (temp === "07") {
+        quarter = "Q3";
+      } else if (temp === "10") {
+        quarter = "Q4";
+      }
+
+      return item[0].substring(0, 4) + " " + quarter;
+    });
     let allYearsDate = rawData.data.map((item) => new Date(item[0]));
     d3.select("svg")
       .selectAll("rect")
       .data(scaledGDP)
       .enter()
       .append("rect")
+      .attr("class", "bar")
       // Add date attribute
       .attr("data-date", (d, i) => {
         return rawData.data[i][0];
@@ -97,7 +123,7 @@ d3.json(
       // Add y attribute for rect
       .attr("y", (d, i) => height - d + 9)
       // Add bar width to all bar
-      .attr("width", barWidth - 0.2)
+      .attr("width", barWidth - 0.1)
       // Add bar height to all bar
       .attr("height", (d) => d)
       // Fill with color bars %80 and over with red and others black
@@ -110,6 +136,34 @@ d3.json(
         }
       })
       // Because of the position of Axis we give transform attr for bars
-      .attr("transform", "translate(50, 0)");
+      .attr("transform", "translate(50, 0)")
+      //////////////////////////////////
+      ////// CREATE tooltip when mouseover  //////
+      /////////////////////////////////
+
+      .on("mouseover", (d, i) => {
+        tooltip
+          .transition()
+          .duration(250)
+          .style("opacity", 0.8);
+        tooltip
+          .html(
+            years[i] +
+              "<br>" +
+              "$" +
+              datasGDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,") +
+              " Billion"
+          )
+          .attr("data-date", rawData.data[i][0])
+          .style("left", i * barWidth + 30 + "px")
+          .style("top", height - 100 + "px")
+          .style("transform", "translateX(50px)");
+      })
+      .on("mouseout", (d) => {
+        tooltip
+          .transition()
+          .duration(200)
+          .style("opacity", 0);
+      });
   }
 );
